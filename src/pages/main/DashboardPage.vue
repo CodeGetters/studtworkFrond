cacebf079333244078a45a2f77849478c5323e33
Johnson Xin <2775145275@qq.com>
2023-08-28 10:31:36 +0800

    chore: ♻️ adjust dashboard page style and login logic

--- src/pages/main/DashboardPage.vue
+++ src/pages/main/DashboardPage.vue
@@ -1,35 +1,10 @@
-<script setup lang="ts">
-const { locale, t } = useI18n();
-import { useI18n } from "vue-i18n";
-import { toggleDark } from "@/core/utils/themeAnimation";
-import { asideTopLayout } from "@/core/helpers/config";
-import { useConfigStore } from "@/store/config";
-
-const configStore = useConfigStore();
-
-/**
- * @description 语言切换
- */
-const changeLang = () => {
-  locale.value === "zh" ? (locale.value = "en") : (locale.value = "zh");
-};
-
-const changeLayout = (): void => {
-  configStore.setDefaultConfigProperty(
-    "mainLayout.asideTop",
-    !asideTopLayout.value,
-  );
-};
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div id="dashboardCom">
-    <button @click="changeLayout()">切换布局</button>
-    <span bg-red>{{ t("hello") }}</span>
-    <button @click="changeLang()">语言切换{{ locale }}</button>
-    <button @click="toggleDark">主题切换</button>
-    <span class="text-red">当前页面 home</span>
-    <router-link to="/404" text-black>前往 notFound</router-link>
+  <div id="dashboardCom" flex flex-row>
+    <!-- 1640 -->
+    <div class="main w78% mr2%">左</div>
+    <div class="aside w20%">右</div>
   </div>
 </template>
 
